2c7716746fa6e0d6cf2ebb139515893e3db40e00
Finn Ove Lium <finnove@me.com>
2012-04-19 16:17:46 +0200

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,7 +5,6 @@ describe "Static pages" do
     subject { page }
     
     describe "Home page" do
-        click_link 
         before { visit root_path }        
         it { should have_selector('h1',    text: 'Sample App') }
         it { should have_selector('title', text: full_title('')) }
