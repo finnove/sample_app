fb3d12d5262351cdd857072c326f2053fbe07bb8
Finn Ove Lium <finnove@me.com>
2012-04-21 12:36:06 +0200

    Finish sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ActiveRecord::Base
     uniqueness: { case_sensitive: false }
     validates :password, length: { minimum: 6 }
     validates :password_confirmation, presence: true
+    
+    before_save { |user| user.email = email.downcase }
+    before_save :create_remember_token
+    
+    private
+    
+    def create_remember_token
+        self.remember_token = SecureRandom.urlsafe_base64
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,7 @@ describe User do
     it { should respond_to(:password_digest) }
     it { should respond_to(:password) }
     it { should respond_to(:password_confirmation) }    
+    it { should respond_to(:remember_token) }
     it { should be_valid }
     
     describe "when name is not present" do
@@ -90,6 +91,11 @@ describe User do
                 @user.should be_valid
             end      
         end
+    end
+    
+    describe "remember token" do
+        before { @user.save }
+        its(:remember_token) { should_not be_blank }
     end
 end
     
